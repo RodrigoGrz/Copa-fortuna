99857ac046ec133292fb7f84216724467d6dd72f
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2022-11-02 13:45:16 -0300

    feat: Finishing db tables and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -19,6 +21,39 @@ async function bootstrap() {
         const pools = await prisma.pool.count();
 
         return { pools };
+    });
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+        
+        const { title } = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+
+        const code = String(generate()).toLocaleUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return reply.status(201).send({ code });
+    });
+
+    fastify.get('/users/count', async () => {
+        const users = await prisma.user.count();
+
+        return { users };
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const guesses = await prisma.guess.count();
+
+        return { guesses };
     });
 
     await fastify.listen({ port: 3333, host: '0.0.0.0' });
